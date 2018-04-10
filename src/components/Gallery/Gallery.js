3dd56dfdebbc4a7c9b9c15d3f8c152c117078ef1
talbenmoshe <talbm@wix.com>
2018-04-10 12:07:21 +0300

    first commit

---
+++ src/components/Gallery/Gallery.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Image from '../Image';
+import './Gallery.scss';
+
+class Gallery extends React.Component {
+  static propTypes = {
+    tag: PropTypes.string
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      images: [],
+      galleryWidth: this.getGalleryWidth()
+    };
+  }
+
+  getGalleryWidth(){
+    try {
+      return document.body.clientWidth;
+    } catch (e) {
+      return 1000;
+    }
+  }
+
+  getImages(tag) {
+    // TODO: Get images from Flickr
+  }
+
+  componentDidMount() {
+    this.getImages(this.props.tag);
+    this.setState({
+      galleryWidth: document.body.clientWidth
+    });
+  }
+
+  componentWillReceiveProps(props) {
+    this.getImages(props.tag);
+  }
+
+  render() {
+    return (
+      <div className="gallery-root">
+        {this.state.images.map(dto => {
+          return <Image key={'image-' + dto.id} dto={dto} galleryWidth={this.state.galleryWidth}/>;
+        })}
+      </div>
+    );
+  }
+}
+
+export default Gallery;
