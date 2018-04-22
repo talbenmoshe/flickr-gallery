ef28b6cb97803568fefce6d8d832b2f7f9c8bfc9
Rami Jarrar <srax47@gmail.com>
2018-04-22 17:42:24 +0300

    Task#2: Add image actions (delete, rotate, expand)

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types';
 import Image from '../Image';
 import './Gallery.scss';
 import flicker from '../../flicker'
+import Modal from 'react-modal';
+
+Modal.setAppElement('#app');
 
 class Gallery extends React.Component {
   static propTypes = {
@@ -13,7 +16,11 @@ class Gallery extends React.Component {
     super(props);
     this.state = {
       images: [],
-      galleryWidth: this.getGalleryWidth()
+      galleryWidth: this.getGalleryWidth(),
+      modalIsOpen: false,
+      modalTitle: '',
+      modalImage: '',
+      modalRotation: 0
     };
   }
 
@@ -37,6 +44,25 @@ class Gallery extends React.Component {
     });
   }
 
+  deleteImage(image) {
+    this.setState({
+      images: this.state.images.filter(img => img !== image)
+    });
+  }
+
+  onExpand(image) {
+    this.setState({
+      modalIsOpen: true,
+      modalTitle: image.title,
+      modalImage: image.link,
+      modalRotation: image.rotation
+    });
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
+
   componentDidMount() {
     this.getImages(this.props.tag);
     this.setState({
@@ -52,8 +78,19 @@ class Gallery extends React.Component {
     return (
       <div className="gallery-root">
         {this.state.images.map(dto => {
-          return <Image key={'image-' + dto.id} dto={dto} galleryWidth={this.state.galleryWidth}/>;
+          return <Image key={'image-' + dto.id} dto={dto} galleryWidth={this.state.galleryWidth}
+                        onDelete={this.deleteImage.bind(this)} onExpand={this.onExpand.bind(this)}/>;
         })}
+
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onRequestClose={this.closeModal.bind(this)}
+          contentLabel={this.state.modalTitle}>
+          <button className="modal-close" onClick={this.closeModal.bind(this)}>x</button>
+          <img src={this.state.modalImage} width="100%" height="100%" style={{
+              transform: `rotate(${this.state.modalRotation}deg)`
+            }}/>
+        </Modal>
       </div>
     );
   }
--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -6,14 +6,16 @@ import './Image.scss';
 class Image extends React.Component {
   static propTypes = {
     dto: PropTypes.object,
-    galleryWidth: PropTypes.number
+    galleryWidth: PropTypes.number,
+    onDelete: PropTypes.func
   };
 
   constructor(props) {
     super(props);
     this.calcImageSize = this.calcImageSize.bind(this);
     this.state = {
-      size: 200
+      size: 200,
+      rotation: 0
     };
   }
 
@@ -35,20 +37,35 @@ class Image extends React.Component {
     return `https://farm${dto.farm}.staticflickr.com/${dto.server}/${dto.id}_${dto.secret}.jpg`;
   }
 
+  onDelete() {
+    this.props.onDelete(this.props.dto);
+  }
+
+  onRotate() {
+    this.setState({
+      rotation: (this.state.rotation + 90) % 360
+    });
+  }
+
+  onExpand() {
+    this.props.onExpand({
+      title: this.props.dto.title,
+      link: this.urlFromDto(this.props.dto),
+      rotation: this.state.rotation
+    });
+  }
+
   render() {
     return (
-      <div
-        className="image-root"
-        style={{
-          backgroundImage: `url(${this.urlFromDto(this.props.dto)})`,
-          width: this.state.size + 'px',
-          height: this.state.size + 'px'
-        }}
-        >
+      <div className="image-root">
+        <img src={this.urlFromDto(this.props.dto)} width={this.state.size + 'px'} height={this.state.size + 'px'}
+             style={{
+               transform: `rotate(${this.state.rotation}deg)`
+             }}/>
         <div>
-          <FontAwesome className="image-icon" name="sync-alt" title="rotate"/>
-          <FontAwesome className="image-icon" name="trash-alt" title="delete"/>
-          <FontAwesome className="image-icon" name="expand" title="expand"/>
+          <FontAwesome className="image-icon" name="sync-alt" title="rotate" onClick={this.onRotate.bind(this)}/>
+          <FontAwesome className="image-icon" name="trash-alt" title="delete" onClick={this.onDelete.bind(this)}/>
+          <FontAwesome className="image-icon" name="expand" title="expand" onClick={this.onExpand.bind(this)}/>
         </div>
       </div>
     );
