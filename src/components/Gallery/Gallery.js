f4f353fffb5589217fe4b2cbf6ca3aea1b6c9dbd
Remah jubran <jubranodesign@gmail.com>
2018-05-23 16:07:02 +0300

    task done

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Image from '../Image';
+import $ from 'jquery';
 import './Gallery.scss';
 
 class Gallery extends React.Component {
@@ -12,11 +13,34 @@ class Gallery extends React.Component {
     super(props);
     this.state = {
       images: [],
-      galleryWidth: this.getGalleryWidth()
+      bigImgPath: null,
+      isImgExpanded: false,
+      galleryWidth: this.getGalleryWidth(),
+      currentPage: 0,
+      isLoadMore: false
     };
+
+    window.addEventListener('resize', () => {
+      this.setState({
+        galleryWidth: document.body.clientWidth
+      });
+    });
+
+    document.addEventListener('scroll', () => {
+      this.setState({
+        isLoadMore: true
+      });
+      if ($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+        this.setState({
+          currentPage: this.state.currentPage + 1
+        });
+        this.getImages(props.tag, this.state.currentPage);
+      }
+    });
+
   }
 
-  getGalleryWidth(){
+  getGalleryWidth() {
     try {
       return document.body.clientWidth;
     } catch (e) {
@@ -25,7 +49,20 @@ class Gallery extends React.Component {
   }
 
   getImages(tag) {
-    // TODO: Get images from Flickr
+    let page = this.state.currentPage;
+
+    fetch('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=2d0066a0ae538617011e54ba902fd4c7&format=json&&nojsoncallback=true&&tags=' + tag + '&page=' + page, {
+      method: 'GET'
+    }).then(res => res.json())
+      .then(res => {
+        if (res.photos) {
+          if (!this.state.isLoadMore) {
+            this.setState({images: res.photos.photo});
+          } else {
+            this.setState({images: this.state.images.concat(res.photos.photo)});
+          }
+        }
+      });
   }
 
   componentDidMount() {
@@ -36,14 +73,40 @@ class Gallery extends React.Component {
   }
 
   componentWillReceiveProps(props) {
+    this.setState({
+      isLoadMore: false,
+      currentPage: 0
+    });
     this.getImages(props.tag);
+  }
+
+  urlFromDto(dto) {
+    return `https://farm${dto.farm}.staticflickr.com/${dto.server}/${dto.id}_${dto.secret}.jpg`;
+  }
+
+  expandImg = (dto) => {
+    this.setState({
+      isImgExpanded: true,
+      bigImgPath: this.urlFromDto(dto)
+    });
+  }
+
+  hideBigImg = () => {
+    this.setState({
+      isImgExpanded: false
+    });
   }
 
   render() {
     return (
       <div className="gallery-root">
-        {this.state.images.map(dto => {
-          return <Image key={'image-' + dto.id} dto={dto} galleryWidth={this.state.galleryWidth}/>;
+        {this.state.isImgExpanded ?
+          <div className="big-img"><span className="close" onClick={this.hideBigImg}> close</span> <img
+            src={this.state.bigImgPath}/></div> : null}
+
+        {this.state.images.map((dto, index) => {
+          return <Image key={'image-' + dto.id} dto={dto} galleryWidth={this.state.galleryWidth}
+                        onExpandImg={this.expandImg} imgIndex={index}/>
         })}
       </div>
     );
