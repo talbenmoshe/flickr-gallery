b9bf2263da9e15b73ffbf2ef33e1804c925f89cf
Abed Shehadeh <abd.sm@hotmail.com>
2018-06-12 02:11:45 +0300

    Share Image spec updated

--- src/components/ShareImage/ShareImage.spec.js
+++ src/components/ShareImage/ShareImage.spec.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import {expect} from 'chai';
-import './ShareImage.scss';
+import ShareImage from './ShareImage.js';
 
 describe('ShareImage', () => {
 
-  const url = 'https://farm1.staticflickr.com/878/41398916355_fbd5bf1b7b.jpg';
+  const imageUrl = 'https://farm1.staticflickr.com/878/41398916355_fbd5bf1b7b.jpg';
   const iconSize = 32;
 
   const mountShareImage = () => {
     return shallow(
-      <ShareImage url={url} iconSize={iconSize}/>,
+      <ShareImage url={imageUrl} iconSize={iconSize}/>,
       {lifecycleExperimental: true, attachTo: document.createElement('div')}
     );
   };
