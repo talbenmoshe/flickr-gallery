672b2446751452f17cb44336ebe7a36ee9b30fa8
Firas Massod <firas.m@vidmatic.tv>
2018-07-01 00:22:50 +0300

    add rotate button functionality

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -13,8 +13,12 @@ class Image extends React.Component {
     super(props);
     this.calcImageSize = this.calcImageSize.bind(this);
     this.state = {
-      size: 200
+      size: 200,
+      rotation: 0
     };
+    this.rotateImage = this.rotateImage.bind(this);
+/*    this.deleteImage = this.deleteImage.bind(this);*/
+    this.expandImage = this.expandImage.bind(this);
   }
 
   calcImageSize() {
@@ -35,20 +39,39 @@ class Image extends React.Component {
     return `https://farm${dto.farm}.staticflickr.com/${dto.server}/${dto.id}_${dto.secret}.jpg`;
   }
 
+  rotateImage() {
+    let rotationAngle = (this.state.rotation + 90) % 360;
+    this.setState({rotation: (rotationAngle)});
+  }
+
+  deleteImage() {
+    console.log(this);
+  }
+
+  expandImage() {
+    console.log(this);
+  }
+
   render() {
     return (
       <div
         className="image-root"
         style={{
-          backgroundImage: `url(${this.urlFromDto(this.props.dto)})`,
           width: this.state.size + 'px',
-          height: this.state.size + 'px'
+          height: this.state.size + 'px',
         }}
         >
+        <span
+          className='img-container'
+          style ={{
+            backgroundImage: `url(${this.urlFromDto(this.props.dto)})`,
+            transform: 'rotate(' + this.state.rotation + 'deg)'
+          }}
+        />
         <div>
-          <FontAwesome className="image-icon" name="sync-alt" title="rotate"/>
-          <FontAwesome className="image-icon" name="trash-alt" title="delete"/>
-          <FontAwesome className="image-icon" name="expand" title="expand"/>
+          <FontAwesome className="image-icon" onClick={this.rotateImage} name="sync-alt" title="rotate"/>
+          <FontAwesome className="image-icon" onClick={this.deleteImage}name="trash-alt" title="delete"/>
+          <FontAwesome className="image-icon" onClick={this.expandImage} name="expand" title="expand"/>
         </div>
       </div>
     );
